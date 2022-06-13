156345cac271e68300a6935a098848f3ef4b3d9b
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-06-13 14:38:31 -0400

    Stock Problem Solved

--- BestTimeToBuyStock.js
+++ BestTimeToBuyStock.js
@@ -10,3 +10,27 @@
 // Output: 5
 // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 // Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+
+var maxProfit = function(prices) {
+    let lowest = prices[0];
+  console.log(lowest)
+    let maxProfit = 0;
+  
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] <= prices[i - 1]) {
+            // Stock down
+          console.log(lowest)
+          console.log(prices[i], i)
+            lowest = Math.min(lowest, prices[i]);
+            console.log(lowest)
+        } else {
+            // Stock up
+            maxProfit = Math.max(maxProfit, prices[i] - lowest);
+        }
+    }
+    
+    return maxProfit;
+};
+
+console.log(maxProfit([1,3,2,7]))
