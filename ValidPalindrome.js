42af8a11d8e7d6116608d2e06bc8a89c1f3a382d
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-06-14 09:15:41 -0400

    Valid Palindrome Initialized

---
+++ ValidPalindrome.js
@@ -0,0 +1,15 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. 
+// Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+
+var isPalindrome = function(s) {
+    
+};
