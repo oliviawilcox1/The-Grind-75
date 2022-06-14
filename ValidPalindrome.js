97e2165532919e89527e8cafeaa52dd8dacc77fd
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-06-14 09:39:20 -0400

    Completed Valid Palindrome

--- ValidPalindrome.js
+++ ValidPalindrome.js
@@ -10,6 +10,14 @@
 // Output: false
 // Explanation: "raceacar" is not a palindrome.
 
+// My Way
 var isPalindrome = function(s) {
-    
-};
+    let word = s.toLowerCase().replace(/[^a-z]/gi, '');
+      let newWord = []
+      for (let i = word.length-1; i >= 0; i--) {
+      newWord.push(word[i])
+      }
+    joinedReverseWord = newWord.join('')
+   return joinedReverseWord===word ? true : false;
+  };
+  console.log(isPalindrome("Hello there:"))
