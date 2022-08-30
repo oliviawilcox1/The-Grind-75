420baa6ab5e18f364ceaca7d2695ecaec9a73ec0
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-08-30 14:26:40 -0400

    Second Way to Solve Isometric String with Dictionary

--- IsomorphicStrings.js
+++ IsomorphicStrings.js
@@ -13,3 +13,34 @@ var isIsomorphic = function(s, t) {
 };
 
 console.log(isIsomorphic("foo", "bar"))
+
+
+
+var isIsomorphic = function(s, t) {
+       
+    let map = new Map()
+    let dicMap2 = {}
+
+    for (var i = 0; i < s.length; i++) {
+       if (!(s[i] in map)) {
+              map[s[i]] = t[i];
+        } else {
+            if (map[s[i]] !== t[i]) {
+                  return false;
+        }
+      }
+    }
+  
+     for (let i = 0; i < t.length; i++) {
+          if (!(t[i] in dicMap2)) {
+              dicMap2[t[i]] = s[i];
+          } else {
+              if (dicMap2[t[i]] !== s[i]) {
+                  return false;
+              }
+          }
+      }
+      return true;
+  };
+  
+  console.log(isIsomorphic("foo", "bar"))
