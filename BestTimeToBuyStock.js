34f9304132a16bc3ee0ee2a70b981fbc3f8381b9
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-09-02 11:29:14 -0400

    Best Time to Buy and Sell

--- BestTimeToBuyStock.js
+++ BestTimeToBuyStock.js
@@ -34,3 +34,15 @@ var maxProfit = function(prices) {
 };
 
 console.log(maxProfit([1,3,2,7]))
+
+// Even More Succinct
+
+var maxProfit = function(prices) {
+  let result = 0;
+  let min = prices[0];
+  for(let i = 1; i < prices.length; i++) {
+      min = Math.min(prices[i], min);
+      result = Math.max(result, prices[i] - min);
+  }
+  return result;
+};
