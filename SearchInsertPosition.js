90c90342a08561e517f58e2c9dc22429ff7482c3
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-09-19 19:14:16 -0400

    Search Insert Position

--- SearchInsertPosition.js
+++ SearchInsertPosition.js
@@ -16,3 +16,24 @@
 
 // Input: nums = [1,3,5,6], target = 7
 // Output: 4
+
+
+
+var searchInsert = function(nums, target) {
+    var right = nums.length - 1;
+    var left = 0;
+      
+    while (left < right) {
+        var mid = Math.floor((left + right) / 2);
+        if (target === nums[mid]) {
+            return mid;
+        } else if (target < nums[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1
+        }
+    }
+  
+    return target > nums[left] ? left + 1 : left;
+};
+
