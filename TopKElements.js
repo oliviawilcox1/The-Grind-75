ad2e61bf9ace48654b86f1db7d77f5abb0cc65ef
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-10-07 13:18:10 -0400

    TopKElements Solved

---
+++ TopKElements.js
@@ -0,0 +1,39 @@
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+
+var topKFrequent = function(nums, k) {
+    let map = new Map();
+    for(let v of nums) {
+        if(!map.has(v)) {
+            map.set(v, 1);
+        } else {
+            map.set(v, map.get(v) + 1);
+        }
+    }
+
+     
+    let array = [];
+    for(let [key, value] of map) {
+        array.push([key, value]);
+    }
+    
+    array.sort(function(a,b) {
+        return b[1] - a[1];
+    })
+    
+    let result = [];
+    for(let i = 0; i < k; i++) {
+        result.push(array[i][0]);
+    }
+    return result;
+};
