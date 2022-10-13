88b4417da5aab57388854be5767ffcec58ebde99
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-10-13 10:56:34 -0400

    Valid Parantheses Solved Third Way

--- ValidParentheses.js
+++ ValidParentheses.js
@@ -78,3 +78,34 @@ var isValid = function(s) {
     
     return stack.length === 0;
 };
+
+
+//Third Way
+var isValid = function(s) {
+        
+  if (s.length ==0 || s.length%2 != 0){
+      return false;
+  }
+  
+  const closingParentheses = Object.freeze({
+      "(": ")",
+      "{": "}",
+      "[": "]"
+  });
+  
+  const stack = [];
+  
+  for (let i=0; i<s.length; i++) {
+      
+      if (s[i] in closingParentheses) {
+          stack.push(s[i]);
+      
+      }else if( s[i] != closingParentheses[stack.pop()]) {
+          
+          return false;
+      }
+  }
+  
+  return stack.length == 0;
+};
+
