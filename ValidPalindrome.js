d85356f89ec26f40bdfc4b7d665bfe7e948b3a7b
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-10-13 11:25:21 -0400

    Valid palindrome

--- ValidPalindrome.js
+++ ValidPalindrome.js
@@ -12,16 +12,16 @@
 
 // My Way
 var isPalindrome = function(s) {
-    let word = s.toLowerCase().replace(/[^a-z]/gi, '');
-      let newWord = []
-      for (let i = word.length-1; i >= 0; i--) {
-      newWord.push(word[i])
-      }
-    joinedReverseWord = newWord.join('')
-   return joinedReverseWord===word ? true : false;
-  };
-  console.log(isPalindrome("Hello there:"))
-
+  let newString = s.replace(/[^a-z0-9]/gi, '').toLowerCase()
+    let newWord = []
+    for (let i = newString.length-1; i >= 0; i--) {
+      newWord.push(newString[i])
+    }
+  
+  console.log(newWord)
+  let joinedReverseWord = newWord.join('')
+ return joinedReverseWord=== newString ? true : false;
+};
 //   Concatenating 
 var isPalindrome = function(s) {
     let word = s.toLowerCase().replace(/[^a-z]/gi, '');
@@ -33,3 +33,14 @@ var isPalindrome = function(s) {
     return newWord===word ? true : false;
   };
   console.log(isPalindrome("Hello there:"))
+
+
+  // Third Way
+  var isPalindrome = function(s) {
+    let newString = s.replace(/[^a-z0-9]/gi, '').toLowerCase()
+    
+    if (newString === newString.split("").reverse().join("")){
+        return true
+    }
+    return false
+};
