4bc2e5dbbb765ab3b0e1dbba8c4e4389762923d4
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-10-31 12:30:24 -0400

    Another Solution for 2D Matrix

--- SearchA2DMatrix.js
+++ SearchA2DMatrix.js
@@ -15,7 +15,7 @@
 // Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 // Output: false
 
-
+// Time Complexity is 40% faster 
 var searchMatrix = function(matrix, target) {
     //   Initally you can do a nested for loop and loop through every item in the matrix but this will have horrible time complexity 
     // We want to do binary search 
@@ -51,3 +51,40 @@ var searchMatrix = function(matrix, target) {
         }
         return false
     }
+
+
+function searchMatrix(matrix, target) {
+
+    if (matrix.length === 0 || matrix[0].length === 0)  return false
+    
+    const arr = getArr();
+    if (!arr)  return false; 
+    return binarySearch(arr, target) !== null;
+    
+    // This function can be implemented using binary search as well
+    function getArr() {
+        for (let i = 0; i < matrix.length; i++) {
+        let arr = matrix[i];
+        if (arr[0] <= target && target <= arr[arr.length - 1]) {
+            return arr;
+        }
+        }
+        return null;
+    }
+    
+    function binarySearch(arr, t) {
+        let left = 0;
+        let right = arr.length - 1;
+        
+        while (left <= right) {
+        if (arr[left] === t)  return left;
+        if (arr[right] === t) return right;
+    
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] === t) return mid;
+        if (arr[mid] < t) left = mid + 1;
+        else if (arr[mid] > t) right = mid - 1;   
+        }
+        return null;
+    }
+};
