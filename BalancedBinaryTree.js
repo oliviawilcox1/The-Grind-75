58f3dec5f361fcd790bac2c968c3de86f590097f
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-11-02 22:07:21 -0400

    BalancedBinaryTree completed

---
+++ BalancedBinaryTree.js
@@ -0,0 +1,42 @@
+// Given a binary tree, determine if it is height-balanced.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: true
+// Example 2:
+
+
+// Input: root = [1,2,2,3,3,null,null,4,4]
+// Output: false
+// Example 3:
+
+// Input: root = []
+// Output: true
+
+
+
+
+
+
+
+var isBalanced = function(root) {
+    let boolean = true
+    
+    function dfs(root) {
+        if(!root){
+           return 0; 
+        }
+        let left =  dfs(root.left) + 1
+        let right = dfs(root.right) + 1
+        if(Math.abs(left - right) > 1){
+            boolean = false;
+        }
+        return Math.max(left, right)
+    }
+    dfs(root)
+    return boolean
+}
