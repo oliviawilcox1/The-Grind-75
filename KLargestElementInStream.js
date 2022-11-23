93393f1f7dd406618e6125fc3516399750c83317
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-11-22 23:57:38 -0500

    Kth Largest Element Solved

--- KLargestElementInStream.js
+++ KLargestElementInStream.js
@@ -21,3 +21,27 @@
 // kthLargest.add(10);  // return 5
 // kthLargest.add(9);   // return 8
 // kthLargest.add(4);   // return 8
+
+
+
+var KthLargest = function(k, nums) {
+    arr=nums.sort((a,b)=>b-a)
+    n=k
+};
+
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    let done = false
+    for(let q=arr.length-1;q>=0;q--){
+        if(val<arr[q]){
+            arr.splice(q+1,0,val)
+            done = true
+            break
+        }
+    }
+    if (!done)arr.splice(0,0,val)
+    return arr[n-1]
+};
